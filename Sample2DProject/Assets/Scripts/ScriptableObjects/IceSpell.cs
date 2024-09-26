0d62ba0690ed599e7cf623e3e0f763493be5f31a
Fives47 <cph9152@rit.edu>
2024-09-25 21:11:11 -0400

    oops forgot to push the changes

--- Sample2DProject/Assets/Scripts/ScriptableObjects/IceSpell.cs
+++ Sample2DProject/Assets/Scripts/ScriptableObjects/IceSpell.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IceSpell : MonoBehaviour
+public class IceSpell : BaseSpell
 {
+    public override void UseChargedSpell()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UseSpell()
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
