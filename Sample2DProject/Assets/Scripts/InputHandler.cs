857237085f5c5e325b375a70790cc03c46a115c1
Patrick Emmons (RIT Student) <ple5024@rit.edu>
2024-09-30 09:43:44 -0400

    updated player to add hitboxes/hurtboxes etc.
    hitboxes and hurtboxes
    attack animations
    color palette and weapon swap
    hitstop and screenshake

--- Sample2DProject/Assets/Scripts/InputHandler.cs
+++ Sample2DProject/Assets/Scripts/InputHandler.cs
@@ -29,6 +29,21 @@ public class InputHandler : MonoBehaviour
     {
         {Inputs.Left, InputState.UnPressed},
         {Inputs.Right, InputState.UnPressed},
+        {Inputs.Up, InputState.UnPressed},
+        {Inputs.Down, InputState.UnPressed},
+        {Inputs.Jump, InputState.UnPressed},
+        {Inputs.Attack, InputState.UnPressed},
+        {Inputs.Grapple, InputState.UnPressed},
+        {Inputs.Shield, InputState.UnPressed},
+        {Inputs.Pause, InputState.UnPressed},
+    };
+
+    public Dictionary<Inputs, InputState> prevInputs = new Dictionary<Inputs, InputState>()
+    {
+        {Inputs.Left, InputState.UnPressed},
+        {Inputs.Right, InputState.UnPressed},
+        {Inputs.Up, InputState.UnPressed},
+        {Inputs.Down, InputState.UnPressed},
         {Inputs.Jump, InputState.UnPressed},
         {Inputs.Attack, InputState.UnPressed},
         {Inputs.Grapple, InputState.UnPressed},
@@ -43,12 +58,12 @@ public class InputHandler : MonoBehaviour
         //set left and right inputs
         if (movementVector.x < 0)
         {
-            keyBindings[Inputs.Left] = InputState.Pressed;
+            keyBindings[Inputs.Left] = InputState.Held;
             keyBindings[Inputs.Right] = InputState.UnPressed;
         }
         else if (movementVector.x > 0)
         {
-            keyBindings[Inputs.Right] = InputState.Pressed;
+            keyBindings[Inputs.Right] = InputState.Held;
             keyBindings[Inputs.Left] = InputState.UnPressed;
         }
         else
@@ -60,12 +75,12 @@ public class InputHandler : MonoBehaviour
         //set up and down inputs
         if (movementVector.y < 0)
         {
-            keyBindings[Inputs.Down] = InputState.Pressed;
+            keyBindings[Inputs.Down] = InputState.Held;
             keyBindings[Inputs.Up] = InputState.UnPressed;
         }
         else if (movementVector.y > 0)
         {
-            keyBindings[Inputs.Up] = InputState.Pressed;
+            keyBindings[Inputs.Up] = InputState.Held;
             keyBindings[Inputs.Down] = InputState.UnPressed;
         }
         else
@@ -85,96 +100,147 @@ public class InputHandler : MonoBehaviour
         switch (button.name)
         {
             case "Jump":
-                if (context.started)
+                if (context.performed)
                 {
-                    keyBindings[Inputs.Jump] = InputState.Pressed;
-                }
-                else if (context.canceled)
-                {
-                    keyBindings[Inputs.Jump] = InputState.Released;
-                }
-                else if (context.performed)
-                {
-                    keyBindings[Inputs.Jump] = InputState.Held;
+                    if(prevInputs[Inputs.Jump] == InputState.UnPressed || prevInputs[Inputs.Jump] == InputState.Released)
+                    {
+                        keyBindings[Inputs.Jump] = InputState.Pressed;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Jump] = InputState.Held;
+                    }
+                        
                 }
                 else
                 {
-                    keyBindings[Inputs.Jump] = InputState.UnPressed;
+                    if(prevInputs[Inputs.Jump] == InputState.Pressed || prevInputs[Inputs.Jump] == InputState.Held)
+                    {
+                        keyBindings[Inputs.Jump] = InputState.Released;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Jump] = InputState.UnPressed;
+                    }
                 }
                 break;
             case "Attack":
-                if (context.started)
+                if (context.performed)
                 {
-                    keyBindings[Inputs.Attack] = InputState.Pressed;
-                }
-                else if (context.canceled)
-                {
-                    keyBindings[Inputs.Attack] = InputState.Released;
-                }
-                else if (context.performed)
-                {
-                    keyBindings[Inputs.Attack] = InputState.Held;
+                    if (prevInputs[Inputs.Attack] == InputState.UnPressed || prevInputs[Inputs.Attack] == InputState.Released)
+                    {
+                        keyBindings[Inputs.Attack] = InputState.Pressed;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Attack] = InputState.Held;
+                    }
+
                 }
                 else
                 {
-                    keyBindings[Inputs.Attack] = InputState.UnPressed;
+                    if (prevInputs[Inputs.Attack] == InputState.Pressed || prevInputs[Inputs.Attack] == InputState.Held)
+                    {
+                        keyBindings[Inputs.Attack] = InputState.Released;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Attack] = InputState.UnPressed;
+                    }
                 }
                 break;
             case "Grapple":
-                if (context.started)
+                if (context.performed)
                 {
-                    keyBindings[Inputs.Grapple] = InputState.Pressed;
-                }
-                else if (context.canceled)
-                {
-                    keyBindings[Inputs.Grapple] = InputState.Released;
-                }
-                else if (context.performed)
-                {
-                    keyBindings[Inputs.Grapple] = InputState.Held;
+                    if (prevInputs[Inputs.Grapple] == InputState.UnPressed || prevInputs[Inputs.Grapple] == InputState.Released)
+                    {
+                        keyBindings[Inputs.Grapple] = InputState.Pressed;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Grapple] = InputState.Held;
+                    }
+
                 }
                 else
                 {
-                    keyBindings[Inputs.Grapple] = InputState.UnPressed;
+                    if (prevInputs[Inputs.Grapple] == InputState.Pressed || prevInputs[Inputs.Grapple] == InputState.Held)
+                    {
+                        keyBindings[Inputs.Grapple] = InputState.Released;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Grapple] = InputState.UnPressed;
+                    }
                 }
                 break;
             case "Shield":
-                if (context.started)
+                if (context.performed)
                 {
-                    keyBindings[Inputs.Shield] = InputState.Pressed;
-                }
-                else if (context.canceled)
-                {
-                    keyBindings[Inputs.Shield] = InputState.Released;
-                }
-                else if (context.performed)
-                {
-                    keyBindings[Inputs.Shield] = InputState.Held;
+                    if (prevInputs[Inputs.Shield] == InputState.UnPressed || prevInputs[Inputs.Shield] == InputState.Released)
+                    {
+                        keyBindings[Inputs.Shield] = InputState.Pressed;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Shield] = InputState.Held;
+                    }
+
                 }
                 else
                 {
-                    keyBindings[Inputs.Shield] = InputState.UnPressed;
+                    if (prevInputs[Inputs.Shield] == InputState.Pressed || prevInputs[Inputs.Shield] == InputState.Held)
+                    {
+                        keyBindings[Inputs.Shield] = InputState.Released;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Shield] = InputState.UnPressed;
+                    }
                 }
                 break;
             case "Pause":
-                if (context.started)
+                if (context.performed)
                 {
-                    keyBindings[Inputs.Pause] = InputState.Pressed;
-                }
-                else if (context.canceled)
-                {
-                    keyBindings[Inputs.Pause] = InputState.Released;
-                }
-                else if (context.performed)
-                {
-                    keyBindings[Inputs.Pause] = InputState.Held;
+                    if (prevInputs[Inputs.Pause] == InputState.UnPressed || prevInputs[Inputs.Pause] == InputState.Released)
+                    {
+                        keyBindings[Inputs.Pause] = InputState.Pressed;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Pause] = InputState.Held;
+                    }
+
                 }
                 else
                 {
-                    keyBindings[Inputs.Pause] = InputState.UnPressed;
+                    if (prevInputs[Inputs.Pause] == InputState.Pressed || prevInputs[Inputs.Pause] == InputState.Held)
+                    {
+                        keyBindings[Inputs.Pause] = InputState.Released;
+                    }
+                    else
+                    {
+                        keyBindings[Inputs.Pause] = InputState.UnPressed;
+                    }
                 }
                 break;
         }
     }
 
+    public void FixedUpdate()
+    {
+        List<Inputs> keys = new List<Inputs>(keyBindings.Keys);
+        foreach (Inputs key in keys)
+        {
+            prevInputs[key] = keyBindings[key];
+            if (keyBindings[key] == InputState.Pressed)
+            {
+                keyBindings[key] = InputState.Held;
+            }
+            if (keyBindings[key] == InputState.Released)
+            {
+                keyBindings[key] = InputState.UnPressed;
+            }
+        }
+    }
 }
