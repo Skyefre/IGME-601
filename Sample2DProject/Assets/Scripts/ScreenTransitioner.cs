81f377ef1a4baebcc4cab90a683aa4eaef255c1e
Fives47 <cph9152@rit.edu>
2024-12-08 20:35:39 -0500

    removed weird call

--- Sample2DProject/Assets/Scripts/ScreenTransitioner.cs
+++ Sample2DProject/Assets/Scripts/ScreenTransitioner.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ScreenTransitioner : MonoBehaviour
 {
